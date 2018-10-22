bd44513a71f95275466efb4272fb50fbe8de48d1
MachariaKuria <skuria@student.cscc.edu>
2018-10-21 20:24:57 -0400

    Added CSS

--- src/main/java/org/wecancodeit/reviews/CategoryPopulator.java
+++ src/main/java/org/wecancodeit/reviews/CategoryPopulator.java
@@ -28,14 +28,24 @@ public class CategoryPopulator implements CommandLineRunner {
 		Review lakeSide = new Review("Customer Service","****","Prompt");
 		lakeSide = reviewRepo.save(lakeSide);
 		
-		Category mountainHotel = new Category("Montain Hotels","/images/TreetopsLodge.jpg","Nyeri, Kenya",treeTops,maraSopa);
-		Category beachHotel = new Category("Beach Hotels","/images/AmboseliSerena.jpg","Amboseli, Kenya",maraSopa,lakeSide);
-		Category lakesideHotel = new Category("Lakeside Hotels","/images/MountainLodge.jpg","Nyeri, Kenya",treeTops,lakeSide);
+		Review seaResort = new Review("Beach Front","****","Ocean View");
+		seaResort = reviewRepo.save(seaResort);
+		
+		Review hilton = new Review("Free WiFi","*****","Speed");
+		hilton = reviewRepo.save(hilton);		
+		
+		Category lakesideHotel = new Category("Lakeside Hotels - Mountain Lodge","/images/MountainLodge.jpg","Nanyuki, Kenya",treeTops,lakeSide);
+		Category mountainHotel = new Category("Montain Hotels - TreeTops","/images/TreetopsLodge.jpg","Nyeri, Kenya",treeTops,maraSopa);
+		Category plainHotel = new Category("Plain Hotels - Mara Sopa Lodge","/images/AmboseliSerena.jpg","Amboseli, Kenya",maraSopa,lakeSide);
+		Category beachHotel = new Category("Beach Hotels - Nyali Beach Hotel","/images/NyaliBeach.jpg","Mombasa, Kenya",lakeSide,seaResort);
+		Category luxuryHotel = new Category("City Hotels - Hilton Hotel","/images/Hilton.jpg","New York, USA",seaResort,hilton);
 		
 		mountainHotel = categoryRepo.save(mountainHotel);
-		beachHotel = categoryRepo.save(beachHotel);
+		plainHotel = categoryRepo.save(plainHotel);
 		lakesideHotel = categoryRepo.save(lakesideHotel);
-	
+		beachHotel = categoryRepo.save(beachHotel);
+		luxuryHotel = categoryRepo.save(luxuryHotel);
+		
 	}
 	
 	
